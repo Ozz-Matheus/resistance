4811bc40d08fe0c1cd03275bccf24f44a77780b8
Ozz-Matheus <montesinos.quintana@gmail.com>
2025-07-08 17:54:05 -0600

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cosmic Resistance
+
+Cosmic Resistance es un videojuego de disparos arcade, inspirado en los clásicos, adaptado y extendido por Ozz-Matheus.
+
+Este proyecto se basa en una renderización optimizada para navegadores modernos y jugar en movíl.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Cosmic Resistance</title>
+  <style>
+    body { margin: 0; background: #000; overflow: hidden; }
+  </style>
+</head>
+<body>
+  <div id="phaser-example"></div>
+
+  <!-- Phaser CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.js"></script>
+
+  <!-- Tu juego -->
+  <script type="module" src="{{ asset('scripts/game.js')}}"></script>
+</body>
+</html>
