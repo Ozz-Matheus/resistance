311e32d9751a878a15065ea03fdb032a58de8db8
Ozz-Matheus <montesinos.quintana@gmail.com>
2026-05-26 10:13:55 -0600

    Se han añadido nuevos archivos de audio para los efectos del juego.

--- public/src/scenes/loader.js
+++ public/src/scenes/loader.js
@@ -56,9 +56,9 @@ scene.load.spritesheet('fullscreen-button', './src/images/fullscreen-button.png'
 /* ------------------------------------------------------------------------------------------ */
 
 scene.load.audio('bg-music', './src/audio/bg-music.ogg');
-scene.load.audio('bullet-sound', './src/audio/bullet.mp3');
-scene.load.audio('explosion-sound', './src/audio/explosion.wav');
-scene.load.audio('die-throw', './src/audio/die-throw.ogg');
+scene.load.audio('bullet-sound', './src/audio/bullet-hero.mp3');
+scene.load.audio('die-throw', './src/audio/bullet-enemy.mp3');
+scene.load.audio('explosion-sound', './src/audio/explosion.mp3');
 scene.load.audio('victory-music', './src/audio/victory-music.mp3');
 scene.load.audio('gameover-music', './src/audio/gameover-music.ogg');
 scene.load.audio('level-passed', './src/audio/level-passed.mp3');
